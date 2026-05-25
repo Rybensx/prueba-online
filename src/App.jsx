2e9607e75c311555bfd16e560a73d52c34ce51b0
Rybensx <anderson_arf@hotmail.es>
2026-05-24 18:33:03 -0600

    Primer deploy

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#0f172a",
+        color: "white",
+        fontSize: "3rem",
+        fontFamily: "Arial"
+      }}
+    >
+      Mi primera app online 🚀
+    </div>
+  );
+}
+
+export default App;
