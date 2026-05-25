10a04bec54198f84231359cf7b08f720ddb50422
Rybensx <anderson_arf@hotmail.es>
2026-05-24 21:07:49 -0600

    Cambiar backend a Render

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const API_URL = "https://backend-prueba-production-a3c3.up.railway.app";
+const API_URL = "https://backend-prueba.onrender.com";
 
 function App() {
   const [tareas, setTareas] = useState([]);
